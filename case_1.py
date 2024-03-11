7213d23ced3c37069947800f7c01870dac89d9df
FractalMind94 <159765072+FractalMind94@users.noreply.github.com>
2024-03-11 15:50:17 +0100

    Update case_1.py

--- case_1.py
+++ case_1.py
@@ -1,47 +1,75 @@
 from datetime import date
 
 
-class Bog:
-    def __init__(self, title, forfatter, aarstal, ISBN):
+class Book:
+    def __init__(self, title, author, year, ISBN):
         self.title=title
-        self.forfatter=forfatter
-        self.aarstal=aarstal
+        self.author=author
+        self.year=year
         self.ISBN=ISBN
 
 
-class Bruger:
-    def __init__(self,bruger_id, navn, adresse, laane_status=[]):
-        self.bruger_id=bruger_id
-        self.navn=navn
-        self.adresse=adresse
-        self.laane_status=laane_status
+class User:
+    def __init__(self,user_id, name, address, loan_status=[]):
+        self.user_id=user_id
+        self.name=name
+        self.address=address
+        self.loan_status=loan_status
 
 
 
-def log(funktion):
+def log(function):
     def wrapper(*args, **kwargs):
         print()
-        funktion(*args, **kwargs)
+        function(*args, **kwargs)
+        pass
         print()
     return wrapper
 
 
-class Bibliotek:
+class Library:
     def __init__(self) -> None:
-        self.bogsamling=Bogsamling()
-        self.brugere=BrugerListe()
+        self.books=BookCollection()
+        self.users=UserList()
         self.reservations = []
+        self.loans=[]
 
        # self.reservations.append({'bruger': id, 'book': bog})
     def make_reservation(user, isbn):
 
-        self.reservations.append(Reservation(user, book))
+        self.reservations.append(Reservation(user, isbn))
 
-    @log
-    def laan():
+
+
+    def loan_report(user)
         pass
 
-    def returner():
+    @log
+    def loan(self, user_id, isbn):
+        user = self.users.search_users(user_id)
+        book = self.books.search_books(isbn)
+
+        # TODO Implement for multiple books of the same type
+        for re in self.loans:
+            if book.ISBN == re.book.ISBN:
+                False
+
+        for re in self.reservations:
+            if book.ISBN == re.book.ISBN:
+                if user.user_id == re.user.user_id:
+                    #remove from reservatiohns
+                    break
+                else:
+                    return
+        # TODO add two weeaks to date
+        self.loans.append(Loan(user, book, date.date()))
+
+        make Loan
+        add loan to Library
+        add loan to user
+        pass
+
+    def return_book():
         pass
 
     def notify_reservation():
@@ -51,21 +79,17 @@ class Bibliotek:
         pass
 
 
+class BookCollection:
+    book_path = r"C:/Users/KOM/Desktop/Opgaver/Case1\books.txt"
 
-
-       
-
-
-class Bogsamling:
-    pass
-
-    def read_book_list():
-        with open() as books:
+    def read_book_list(self):
+        with open(self.book_path, "r") as books:
              for book in books:
-                yield book
+                book_object=Book(*book.strip().split(':'))
+                yield book_object
 
 
-    def search_books(isbn):
+    def search_books(self, isbn):
         for book in self.read_book_list():
             if isbn == book.ISBN:
                 return book
@@ -73,32 +97,34 @@ class Bogsamling:
         # SELECT * FROM Books WHERE ISBN equal id
 
 
-
-class BrugerListe:
+class UserList:
     pass
+    user_path = r"C:/Users/KOM/Desktop/Opgaver/Case1\\users.txt"
+    def read_user_list(self):
+        with open(self.user_path, "r") as users:
+             for user in users:
+                user_object = User(*user.strip().split(':'))
+                yield user_object
 
-    def read_bruger_list():
-        with open() as brugere:
-             for bruger in brugere:
-                yield bruger
+    def search_users(self, user_id):
+        for user in self.read_user_list():
+            if user_id == user.user_id:
+                return user
 
-    def search_bruger(bruger_id):
-        for bruger in self.read_bruger_list():
-            if bruger_id == bruger.bruger_id:
-                return bruger
-
-class Udlaan:
-    def __init__(self, bruger, book, expiration_date) -> None:
-        self.bruger = bruger
+class Loan:
+    def __init__(self, user, book, expiration_date) -> None:
+        self.user = user
         self.book = book
-        self.dato = date.date()
+        self.date = date.date()
         self.expiration_date = expiration_date
 
 
 class Reservation:
-    def __init__(self, bruger, book) -> None:
-        self.bruger = bruger
+    def __init__(self, user, book) -> None:
+        self.user = user
         self.book = book
-        self.dato = date.date()
+        self.dat = date.date()
         
 
+class Menu:
+    pass
